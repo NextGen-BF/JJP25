676e7e62c4ecfb28c37d27abc534c5e26fe23aa2
Aleksandar Stoyanov <Aleksandar.Stoyanov@Aleksandar-Stoyanovs-Mac.local>
2025-02-04 09:47:35 +0200

    JJP-134: add separate package for paymentExec in controller

--- app/backend/src/main/java/com/backend/controller/paymentExecution/PaymentExecutionController.java
+++ app/backend/src/main/java/com/backend/controller/paymentExecution/PaymentExecutionController.java
@@ -1,4 +1,4 @@
-package com.backend.controller.payment;
+package com.backend.controller.paymentExecution;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
