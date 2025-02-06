d20c60a9f6b4ae4c9510035da0e9caa3d5a4da6c
Yordan-Radanov <yordan.radanov@blankfactor.com>
2025-02-06 16:17:03 +0200

    JJP-137: Fixed id type

--- app/database/Secure-database-creation.sql
+++ app/database/Secure-database-creation.sql
@@ -1,7 +1,7 @@
 use ems_auth_db;
 
 create table if not exists secure_user_info (
-	id primary key,
+	id int primary key,
 	username varchar(255) unique not null,
 	password varchar(255) not null,
 	birth_date timestamp not null,
