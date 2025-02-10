37799ee899e18ba40c735f9fa832e6531d99d115
Yordan-Radanov <yordan.radanov@blankfactor.com>
2025-02-10 16:21:47 +0200

    JJP-139: Create SideBar component and Redux slice to manage the state of the sidebar

--- app/frontend/src/redux/store.ts
+++ app/frontend/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./slices/usersSlice";
 import eventReducer from "./slices/eventSlice";
+import sidebarReducer from "./slices/sidebarSlice"
 
 export const store = configureStore({
   reducer: {
+    sidebar: sidebarReducer,
     users: usersReducer,
     event: eventReducer
   },
