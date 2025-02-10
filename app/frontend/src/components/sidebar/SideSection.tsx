543dcd29e485106d16377ad7ebcd0402b64a2a98
Yordan-Radanov <yordan.radanov@blankfactor.com>
2025-02-10 16:21:47 +0200

    JJP-139: Create SideBar component and Redux slice to manage the state of the sidebar

---
+++ app/frontend/src/components/sidebar/SideSection.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Box, ListSubheader, SvgIconProps } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { styles } from "./SideBarStyles";
+import { NavLink } from "react-router-dom";
+import "./SideBar.scss"
+
+export interface SidebarItem {
+  icon: React.ElementType<SvgIconProps>;
+  message: string;
+  linkTo: string;
+}
+
+interface SidebarProps {
+  items: SidebarItem[];
+  title: string;
+}
+
+export const SideSection: React.FC<SidebarProps> = ({ items, title }) => {
+  const isOpen = useSelector((state: RootState) => state.sidebar.isOpen);
+
+  const toggleClass = (isActive : boolean): string => {
+    if (isActive) {
+      return isOpen ? "link-active link-open" : "link-active link-closed";
+    } 
+
+    return "link-styles"
+  }
+
+  return (
+    <Box>
+      <ListSubheader
+        sx={styles.listSubheaderStyles}
+      >
+        {title}
+      </ListSubheader>
+      <List>
+        {items.map((item, index) => (
+          <NavLink to={item.linkTo} className={({ isActive }) => toggleClass(isActive)} >
+            <ListItem key={index} disablePadding >
+              <ListItemButton disableRipple className="list-item-button">
+                <ListItemIcon >
+                  <item.icon />
+                </ListItemIcon>
+                {isOpen && (
+                  <ListItemText
+                    primary={item.message}
+                    sx={styles.listItemTextStyles}
+                  />
+                )}
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </Box>
+  );
+};
--- app/frontend/src/redux/store.ts
+++ app/frontend/src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./slices/usersSlice";
+import sidebarReducer from "./slices/sidebarSlice"
 
 export const store = configureStore({
   reducer: {
+    sidebar: sidebarReducer,
     users: usersReducer,
   },
 });
