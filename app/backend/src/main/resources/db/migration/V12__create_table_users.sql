3188068f323b1b88cabdd05d827f7af68a851a1c
PetarMitov <petar.mitov@blankfactor.com>
2025-02-10 17:29:03 +0200

    jjp-133: rename profile_pic to profile_picture in users table

--- app/backend/src/main/resources/db/migration/V12__create_table_users.sql
+++ app/backend/src/main/resources/db/migration/V12__create_table_users.sql
@@ -2,7 +2,7 @@ create table if not exists users (
     id integer not null auto_increment,
     created_at datetime(6) not null,
     phone varchar(15) not null,
-    profile_pic varchar(255),
+    profile_picture varchar(255),
     type enum ('ATTENDEE','ORGANISER') not null,
     updated_at datetime(6) not null,
     primary key (id)
