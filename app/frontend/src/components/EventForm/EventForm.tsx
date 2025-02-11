1b493cd0c7d5a4e14861cddde6b99666a6bea92d
Aleksandar Stoyanov <Aleksandar.Stoyanov@Mac.localstaff>
2025-02-11 14:06:24 +0200

    JJP-141: Add Event Form to be able to test the Calendar and make it work on smaller screens

--- app/frontend/src/components/EventForm/EventForm.tsx
+++ app/frontend/src/components/EventForm/EventForm.tsx
@@ -4,6 +4,7 @@ import { RootState } from "../../redux/store";
 import { useState } from "react";
 import { Dayjs } from "dayjs";
 import { updateEvent } from "../../redux/slices/eventSlice";
+import MultiDatePicker from "../MultiDatePicker/MultiDatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
@@ -23,5 +24,12 @@ const EventForm = () => {
         dispatch(updateEvent({ dates: selectedDates }));
     }
 
-    return;
+    return (
+        <Box sx={{ p: 3, display: "flex", flexDirection: "column", gap: 2 }}>
+            {/* MultiDatePicker Component */}
+            <MultiDatePicker selectedDates={selectedDates} setSelectedDates={setSelectedDates} />
+        </Box>
+    );
 }
+
+export default EventForm;
