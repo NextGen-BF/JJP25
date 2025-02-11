d7cf8c64b02d1c65b07b72e83e1cddd80a6bd125
Aleksandar Stoyanov <Aleksandar.Stoyanov@Aleksandar-Stoyanovs-Mac.local>
2025-02-11 17:50:12 +0200

    JJP-144: Add basic implementation of the Stepper and build upon it

---
+++ app/frontend/src/components/EventStepper/EventStepper.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Stepper, Step, StepLabel, Button, Typography, Box } from '@mui/material';
+import { Event, LocationOn, ConfirmationNumber } from '@mui/icons-material';
+import { EventStepperStyles } from './EventStepperStyles';
+
+const steps = ['Event Creation', 'Venue Creation', 'Ticket Creation'];
+
+const EventStepper: React.FC = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set<number>());
+
+  const isStepOptional = (step: number) => step === 1;
+
+  const isStepSkipped = (step: number) => skipped.has(step);
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={EventStepperStyles.stepperContainer}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: { optional?: React.ReactNode; } = {};
+
+          if (isStepOptional(index)) {
+            labelProps.optional = <Typography variant="caption">Optional</Typography>;
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+
+          const stepLabelColor =
+            index <= activeStep ? EventStepperStyles.stepLabel.active : EventStepperStyles.stepLabel.future;
+          const stepIconColor =
+            index <= activeStep ? EventStepperStyles.stepIcon.active : EventStepperStyles.stepIcon.future;
+
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel
+                {...labelProps}
+                sx={{
+                  '& .MuiStepLabel-label': {
+                    color: stepLabelColor,
+                  },
+                  '& .MuiStepIcon-root': {
+                    color: stepIconColor, 
+                  },
+                }}
+              >
+                {label}
+              </StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you're finished</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )}
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
+};
+
+export default EventStepper;
---
+++ app/frontend/src/constants/EventStepperConstants.tsx
@@ -0,0 +1,4 @@
+export const EventStepperConstants = {
+    ACTIVE_COLOR: "#2b6aff",
+    INACTIVE_COLOR: "#666"
+}
