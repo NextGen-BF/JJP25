72cad42cd46c8aff39a80f55c86d43203e17fdc3
Aleksandar Stoyanov <Aleksandar.Stoyanov@Mac.localstaff>
2025-02-13 09:47:00 +0200

    JJP-147: Add Initial Form Component for Event Creation

---
+++ app/frontend/src/components/EventForm/EventFormStyles.ts
@@ -0,0 +1,52 @@
+export const EventFormStyles = {
+  formContainer: {
+    position: "relative",
+    top: "10vh",
+    width: "90vw",
+    height: "80vh",
+    maxWidth: "1200px",
+    maxHeight: "680px",
+    padding: "20px",
+    backgroundColor: "#f5f5f7",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)",
+  },
+
+  leftBox: {
+    flex: "0 1 40%",
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "500px",
+    maxHeight: "300px",
+    margin: "20px",
+    gap: "16px",
+    width: "50%",
+  },
+
+  rightBox: {
+    flex: "0 1 40%",
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "500px",
+    maxHeight: "300px",
+    margin: "20px",
+    gap: "16px",
+    width: "50%",
+  },
+
+  selectorsBox: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "16px",
+    width: "100%",
+    maxWidth: "600px",
+  },
+
+  backButton: {
+    flex: "0 1 5%", // Takes 5% of space
+  },
+
+  nextButton: {
+    flex: "0 1 5%", // Takes 5% of space
+  },
+};
