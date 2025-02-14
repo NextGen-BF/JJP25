89314455c995a3a25cb7a6c5bc7ccb5a0f01d936
Yordan-Radanov <yordan.radanov@blankfactor.com>
2025-02-14 11:11:06 +0200

    JJP-139: Update App file, Redux store and fix the sidebar height

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,58 +1,94 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar  from './components/Navbar';
-import Footer from './components/Footer/Footer';
-import Login from './pages/LoginPage/LoginPage';
-import HomePage from './pages/HomePage/HomePage';
-import DashboardPage from './pages/DashboardPage/DashboardPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import EventsPage from './pages/EventsPage/EventsPage';
-import TicketsPage from './pages/TicketsPage/TicketsPage';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import NotificationsPage from './pages/NotificationsPage/NotificationsPage';
-import CreateEventPage from './pages/adminPages/CreateEventPage/CreateEventPage';
-import CreateEventSucessPage from './pages/adminPages/CreateEventSucessPage/CreateEventSucessPage';
-import AccountPage from './pages/adminPages/AccountPage/AccountPage';
-import CreateVenuePage from './pages/adminPages/CreateVenuePage/CreateVenuePage';
-import CreateTicketPage from './pages/adminPages/CreateTicketPage/CreateTicketPage';
-import CssBaseline from '@mui/material/CssBaseline';
-import './App.scss'
-import MockUserList from './pages/MockUserList/MockUserList';
-import { Box } from '@mui/material';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import EventsPage from "./pages/EventsPage/EventsPage";
+import TicketsPage from "./pages/TicketsPage/TicketsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import NotificationsPage from "./pages/NotificationsPage/NotificationsPage";
+import CreateEventPage from "./pages/adminPages/CreateEventPage/CreateEventPage";
+import CreateEventSucessPage from "./pages/adminPages/CreateEventSucessPage/CreateEventSucessPage";
+import AccountPage from "./pages/adminPages/AccountPage/AccountPage";
+import CreateVenuePage from "./pages/adminPages/CreateVenuePage/CreateVenuePage";
+import CreateTicketPage from "./pages/adminPages/CreateTicketPage/CreateTicketPage";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.scss";
+import MockUserList from "./pages/MockUserList/MockUserList";
+import { Box } from "@mui/material";
+import SideBar from "./components/sidebar/SideBar";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 export default function App() {
+  const isSideBarOpen = useSelector((state: RootState) => state.sidebar.isOpen);
+
   return (
     <Router>
       <CssBaseline />
-      <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh"}}>
-        <Navbar />
-
-        <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" , paddingTop: "60px"}}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/tickets" element={<TicketsPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/not-found" element={<NotFoundPage />} />
-          <Route path="/mock-users" element={<MockUserList />} />
-          <Route path="*" element={<Navigate to="/not-found" replace />} />
-
-          // Admin routes
-
-          <Route path="/dashboard/event-creation/event" element={<CreateEventPage />}/>
-          <Route path="/dashboard/event-creation/venue" element={<CreateVenuePage />}/>
-          <Route path="/dashboard/event-creation/ticket" element={<CreateTicketPage />}/>
-          <Route path="/dashboard/event-creation/success" element={<CreateEventSucessPage />}/>
-          <Route path="/dashboard/your-events"/>
-          <Route path="/dashboard/your-events-statistics"/>
-          <Route path="/dashboard/rsvp-creation"/>
-          <Route path="/dashboard/account" element={<AccountPage />}/>
-        </Routes>
+      <Navbar />
+      <SideBar />
+      <Box
+        sx={{ 
+          display: "flex", 
+          flexDirection: "column", 
+          minHeight: "100vh", 
+          overflowY: "hidden" }}
+      >
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            paddingTop: "60px",
+            marginLeft: isSideBarOpen ? "260px" : "80px",
+            transition: "0.3s all",
+            zIndex: -1
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/tickets" element={<TicketsPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/not-found" element={<NotFoundPage />} />
+            <Route path="/mock-users" element={<MockUserList />} />
+            <Route path="*" element={<Navigate to="/not-found" replace />} />
+            // Admin routes
+            <Route
+              path="/dashboard/event-creation/event"
+              element={<CreateEventPage />}
+            />
+            <Route
+              path="/dashboard/event-creation/venue"
+              element={<CreateVenuePage />}
+            />
+            <Route
+              path="/dashboard/event-creation/ticket"
+              element={<CreateTicketPage />}
+            />
+            <Route
+              path="/dashboard/event-creation/success"
+              element={<CreateEventSucessPage />}
+            />
+            <Route path="/dashboard/your-events" />
+            <Route path="/dashboard/your-events-statistics" />
+            <Route path="/dashboard/rsvp-creation" />
+            <Route path="/dashboard/account" element={<AccountPage />} />
+          </Routes>
         </Box>
 
-      <Footer/>
+        <Footer />
       </Box>
     </Router>
   );
