0255e306cb6d05138a524ff1ead96ca07498dc61
Aleksandar Stoyanov <Aleksandar.Stoyanov@Mac.localstaff>
2025-02-19 09:31:31 +0200

    JJP-147: Fix duplicated code

--- app/frontend/src/components/EventForm/EventForm.tsx
+++ app/frontend/src/components/EventForm/EventForm.tsx
@@ -22,8 +22,6 @@ const EventForm: React.FC = () => {
     mode: "onChange",
   });
 
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
   const onSubmit = (data: any) => {
     console.log(data);
     toast.success(EventFormConstants.TOAST_MESSAGES.SUCCESS_EVENT_CREATION);
@@ -39,300 +37,131 @@ const EventForm: React.FC = () => {
   return (
     <Box sx={EventFormStyles.formContainer}>
       <form onSubmit={handleSubmit(onSubmit)} className="event-form">
-        {isMobile ? (
-          <>
-            {/* Will use the buttons from the Stepper in the actual Page */}
-            <Box sx={EventFormStyles.buttonContainer}>
-              <Box sx={EventFormStyles.backButton}>
-                <Button variant="outlined">Back</Button>
-              </Box>
+        <Box sx={EventFormStyles.leftBox}>
+          <FormInput
+            name={EventFormConstants.NAMES.EVENT_TITLE}
+            control={control}
+            label={EventFormConstants.LABELS.EVENT_TITLE}
+            required
+            rules={{
+              minLength: {
+                value:
+                  EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH.MESSAGE,
+                message:
+                  EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH.MESSAGE,
+              },
 
-              <Box sx={EventFormStyles.nextButton}>
-                <Button type="submit" variant="contained">
-                  Next
-                </Button>
-              </Box>
-            </Box>
+              pattern: {
+                value: EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN.VALUE,
+                message:
+                  EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN.MESSAGE,
+              },
+            }}
+            error={
+              typeof errors.eventTitle?.message === "string"
+                ? errors.eventTitle.message
+                : undefined
+            }
+            sx={EventFormStyles.backButton}
+          />
 
-            <Box sx={EventFormStyles.leftBox}>
-              <FormInput
-                name={EventFormConstants.NAMES.EVENT_TITLE}
+          <FormInput
+            name={EventFormConstants.NAMES.EVENT_DESCRIPTION}
+            control={control}
+            label={EventFormConstants.LABELS.EVENT_DESCRIPTION}
+            required
+            rules={{
+              minLength: {
+                value:
+                  EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION.MIN_LENGTH
+                    .VALUE,
+                message:
+                  EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION.MIN_LENGTH
+                    .MESSAGE,
+              },
+            }}
+            multiline
+            rows={8}
+            error={
+              typeof errors.eventDescription?.message === "string"
+                ? errors.eventDescription.message
+                : undefined
+            }
+          />
+        </Box>
+
+        <Box sx={EventFormStyles.rightBox}>
+          <Box sx={EventFormStyles.eventDatesBox}>
+            <Typography variant="h6">Event Dates</Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Controller
+                name={EventFormConstants.NAMES.EVENT_DATES}
                 control={control}
-                label={EventFormConstants.LABELS.EVENT_TITLE}
-                required
                 rules={{
-                  minLength: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH
-                        .VALUE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH
-                        .MESSAGE,
-                  },
-
-                  pattern: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN.VALUE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN
-                        .MESSAGE,
-                  },
+                  required: EventFormConstants.VALIDATIONS.EVENT_DATES.REQUIRED,
+                  validate: (value) =>
+                    value.length > 0 ||
+                    EventFormConstants.VALIDATIONS.EVENT_DATES.VALIDATE,
                 }}
-                error={
-                  typeof errors.eventTitle?.message === "string"
-                    ? errors.eventTitle.message
-                    : undefined
-                }
-                sx={EventFormStyles.backButton}
-              />
-
-              <FormInput
-                name={EventFormConstants.NAMES.EVENT_DESCRIPTION}
-                control={control}
-                label={EventFormConstants.LABELS.EVENT_DESCRIPTION}
-                required
-                rules={{
-                  minLength: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION
-                        .MIN_LENGTH.VALUE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION
-                        .MIN_LENGTH.MESSAGE,
-                  },
-                }}
-                multiline
-                rows={8}
-                error={
-                  typeof errors.eventDescription?.message === "string"
-                    ? errors.eventDescription.message
-                    : undefined
-                }
-              />
-            </Box>
-
-            <Box sx={EventFormStyles.rightBox}>
-              <Box sx={EventFormStyles.eventDatesBox}>
-                <Typography variant="h6">
-                  {EventFormConstants.LABELS.EVENT_DATES}
-                </Typography>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <Controller
-                    name={EventFormConstants.NAMES.EVENT_DATES}
-                    control={control}
-                    rules={{
-                      required:
-                        EventFormConstants.VALIDATIONS.EVENT_DATES.REQUIRED,
-                      validate: (value) =>
-                        value.length > 0 ||
-                        EventFormConstants.VALIDATIONS.EVENT_DATES.VALIDATE,
-                    }}
-                    render={({ field }) => (
-                      <MultiDatePicker
-                        selectedDates={field.value || []}
-                        setSelectedDates={(dates: Dayjs[]) =>
-                          field.onChange(dates)
-                        }
-                      />
-                    )}
+                render={({ field }) => (
+                  <MultiDatePicker
+                    selectedDates={field.value || []}
+                    setSelectedDates={(dates: Dayjs[]) => field.onChange(dates)}
                   />
-                </LocalizationProvider>
-
-                {typeof errors.eventDates?.message === "string" && (
-                  <Typography variant="body2" color="error">
-                    {errors.eventDates.message}
-                  </Typography>
                 )}
-              </Box>
-
-              {/* All Enums will have actual values when we start using the Events slice */}
-
-              <Box sx={EventFormStyles.selectorsBox}>
-                <FormAutoComplete
-                  name={EventFormConstants.NAMES.VENUE}
-                  control={control}
-                  rules={venueValidation}
-                  label={EventFormConstants.LABELS.VENUE}
-                  options={EventFormConstants.VALIDATIONS.VENUE.VALID_OPTIONS}
-                  required
-                  error={
-                    typeof errors.venue?.message === "string"
-                      ? errors.venue.message
-                      : undefined
-                  }
-                />
-
-                <FormSelect
-                  name={EventFormConstants.NAMES.CATEGORY}
-                  control={control}
-                  label={EventFormConstants.LABELS.CATEGORY}
-                  options={EventFormConstants.SELECT_OPTIONS.CATEGORIES}
-                  required
-                  error={
-                    typeof errors.category?.message === "string"
-                      ? errors.category.message
-                      : undefined
-                  }
-                />
-
-                <FormSelect
-                  name={EventFormConstants.NAMES.AGE_RESTRICTION}
-                  control={control}
-                  label={EventFormConstants.LABELS.AGE_RESTRICTION}
-                  options={EventFormConstants.SELECT_OPTIONS.AGE_RESTRICTIONS}
-                  error={
-                    typeof errors.ageRestriction?.message === "string"
-                      ? errors.ageRestriction.message
-                      : undefined
-                  }
-                />
-              </Box>
-            </Box>
-          </>
-        ) : (
-          <>
-            <Box sx={EventFormStyles.backButton}>
-              <Button variant="outlined">Back</Button>
-            </Box>
-
-            <Box sx={EventFormStyles.leftBox}>
-              <FormInput
-                name={EventFormConstants.NAMES.EVENT_TITLE}
-                control={control}
-                label={EventFormConstants.LABELS.EVENT_TITLE}
-                required
-                rules={{
-                  minLength: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH
-                        .MESSAGE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.MIN_LENGTH
-                        .MESSAGE,
-                  },
-
-                  pattern: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN.VALUE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_TITLE.PATTERN
-                        .MESSAGE,
-                  },
-                }}
-                error={
-                  typeof errors.eventTitle?.message === "string"
-                    ? errors.eventTitle.message
-                    : undefined
-                }
-                sx={EventFormStyles.backButton}
               />
+            </LocalizationProvider>
 
-              <FormInput
-                name={EventFormConstants.NAMES.EVENT_DESCRIPTION}
-                control={control}
-                label={EventFormConstants.LABELS.EVENT_DESCRIPTION}
-                required
-                rules={{
-                  minLength: {
-                    value:
-                      EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION
-                        .MIN_LENGTH.VALUE,
-                    message:
-                      EventFormConstants.VALIDATIONS.EVENT_DESCRIPTION
-                        .MIN_LENGTH.MESSAGE,
-                  },
-                }}
-                multiline
-                rows={8}
-                error={
-                  typeof errors.eventDescription?.message === "string"
-                    ? errors.eventDescription.message
-                    : undefined
-                }
-              />
-            </Box>
+            {typeof errors.eventDates?.message === "string" && (
+              <Typography variant="body2" color="error">
+                {errors.eventDates.message}
+              </Typography>
+            )}
+          </Box>
 
-            <Box sx={EventFormStyles.rightBox}>
-              <Box sx={EventFormStyles.eventDatesBox}>
-                <Typography variant="h6">Event Dates</Typography>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <Controller
-                    name={EventFormConstants.NAMES.EVENT_DATES}
-                    control={control}
-                    rules={{
-                      required:
-                        EventFormConstants.VALIDATIONS.EVENT_DATES.REQUIRED,
-                      validate: (value) =>
-                        value.length > 0 ||
-                        EventFormConstants.VALIDATIONS.EVENT_DATES.VALIDATE,
-                    }}
-                    render={({ field }) => (
-                      <MultiDatePicker
-                        selectedDates={field.value || []}
-                        setSelectedDates={(dates: Dayjs[]) =>
-                          field.onChange(dates)
-                        }
-                      />
-                    )}
-                  />
-                </LocalizationProvider>
+          <Box sx={EventFormStyles.selectorsBox}>
+            <FormAutoComplete
+              name={EventFormConstants.NAMES.VENUE}
+              control={control}
+              rules={venueValidation}
+              label={EventFormConstants.LABELS.VENUE}
+              options={EventFormConstants.VALIDATIONS.VENUE.VALID_OPTIONS}
+              required
+              error={
+                typeof errors.venue?.message === "string"
+                  ? errors.venue.message
+                  : undefined
+              }
+            />
 
-                {typeof errors.eventDates?.message === "string" && (
-                  <Typography variant="body2" color="error">
-                    {errors.eventDates.message}
-                  </Typography>
-                )}
-              </Box>
+            <FormSelect
+              name={EventFormConstants.NAMES.CATEGORY}
+              control={control}
+              label={EventFormConstants.LABELS.CATEGORY}
+              options={EventFormConstants.SELECT_OPTIONS.CATEGORIES}
+              required
+              error={
+                typeof errors.category?.message === "string"
+                  ? errors.category.message
+                  : undefined
+              }
+            />
 
-              <Box sx={EventFormStyles.selectorsBox}>
-                <FormAutoComplete
-                  name={EventFormConstants.NAMES.VENUE}
-                  control={control}
-                  rules={venueValidation}
-                  label={EventFormConstants.LABELS.VENUE}
-                  options={EventFormConstants.VALIDATIONS.VENUE.VALID_OPTIONS}
-                  required
-                  error={
-                    typeof errors.venue?.message === "string"
-                      ? errors.venue.message
-                      : undefined
-                  }
-                />
+            <FormSelect
+              name={EventFormConstants.NAMES.AGE_RESTRICTION}
+              control={control}
+              label={EventFormConstants.LABELS.AGE_RESTRICTION}
+              options={EventFormConstants.SELECT_OPTIONS.AGE_RESTRICTIONS}
+              error={
+                typeof errors.ageRestriction?.message === "string"
+                  ? errors.ageRestriction.message
+                  : undefined
+              }
+            />
+          </Box>
+        </Box>
 
-                <FormSelect
-                  name={EventFormConstants.NAMES.CATEGORY}
-                  control={control}
-                  label={EventFormConstants.LABELS.CATEGORY}
-                  options={EventFormConstants.SELECT_OPTIONS.CATEGORIES}
-                  required
-                  error={
-                    typeof errors.category?.message === "string"
-                      ? errors.category.message
-                      : undefined
-                  }
-                />
-
-                <FormSelect
-                  name={EventFormConstants.NAMES.AGE_RESTRICTION}
-                  control={control}
-                  label={EventFormConstants.LABELS.AGE_RESTRICTION}
-                  options={EventFormConstants.SELECT_OPTIONS.AGE_RESTRICTIONS}
-                  error={
-                    typeof errors.ageRestriction?.message === "string"
-                      ? errors.ageRestriction.message
-                      : undefined
-                  }
-                />
-              </Box>
-            </Box>
-
-            <Box sx={EventFormStyles.nextButton}>
-              <Button type="submit" variant="contained">
-                Next
-              </Button>
-            </Box>
-          </>
-        )}
+        <Box sx={EventFormStyles.nextButton}></Box>
       </form>
     </Box>
   );
