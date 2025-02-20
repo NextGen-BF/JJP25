8ebf9079c1919ab0874ac10eb1fdcdc59f984033
Aleksandar Stoyanov <Aleksandar.Stoyanov@Mac.localstaff>
2025-02-20 17:52:01 +0200

    JJP-148: Create Venue form and add styles for it

---
+++ app/frontend/src/components/VenueForm/VenueForm.tsx
@@ -0,0 +1,176 @@
+import { useForm, Controller } from "react-hook-form";
+import { Box, Button, Typography } from "@mui/material";
+import { VenueFormStyles } from "./VenueFormStyles";
+import { VenueFormConstants } from "../../constants/VenueFormConstants";
+import "./VenueFormStyles.scss";
+import FormSelect from "../FormSelect/FormSelect";
+import FormInput from "../FormInput/FormInput";
+import FormAutoComplete from "../FormAutoComplete/FormAutoComplete";
+import { toast } from "react-toastify";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { FC } from "react";
+import {
+  Venue,
+  VenueType,
+  createVenue,
+  updateVenue,
+} from "../../redux/slices/venueSlice";
+import ButtonComponent from "../button/ButtonComponent";
+
+const VenueForm: FC = () => {
+  const dispatch = useDispatch();
+  const { venue, isNewVenue } = useSelector((state: RootState) => state.venue);
+  const venueTypes = Object.values(VenueType);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+    setValue,
+  } = useForm({
+    defaultValues: venue,
+    mode: "onChange",
+    reValidateMode: "onSubmit",
+  });
+
+  const handleChange = (field: keyof Venue, value: any) => {
+    dispatch(updateVenue({ [field]: value }));
+    setValue(field, value);
+  };
+
+  const initialVenueRef = useRef<Venue>(venue);
+  const isInitialRender = useRef(true);
+
+  useEffect(() => {
+    if (isInitialRender.current) {
+      initialVenueRef.current = venue;
+      isInitialRender.current = false;
+    }
+  }, [venue]);
+
+  const onSubmit = async (venue: Venue) => {
+    const isFormValid = await trigger();
+
+    if (!isFormValid) {
+      toast.error("Please fill in all required fields correctly.");
+      return;
+    }
+
+    if (!isSubmitted) {
+      dispatch(createVenue(venue));
+      toast.success(VenueFormConstants.TOAST_MESSAGES.SUCCESS_VENUE_CREATION);
+      setIsSubmitted(true);
+    } else {
+      console.log("venue", venue);
+      const isVenueChanged =
+        JSON.stringify(venue) !== JSON.stringify(initialVenueRef.current);
+
+      if (isVenueChanged) {
+        dispatch(updateVenue(venue));
+        toast.info(VenueFormConstants.TOAST_MESSAGES.SUCCESS_VENUE_UPDATE);
+      }
+    }
+  };
+
+  return (
+    <Box sx={VenueFormStyles.formContainer}>
+      <form onSubmit={handleSubmit(onSubmit)} className="event-form">
+        <Box sx={VenueFormStyles.leftBox}>
+          <FormInput
+            name={VenueFormConstants.NAMES.VENUE_NAME}
+            control={control}
+            label={VenueFormConstants.LABELS.VENUE_NAME}
+            defaultValue={venue.name}
+            required
+            onChange={(e) => handleChange("name", e.target.value)}
+            error={errors.name?.message as string}
+          />
+
+          <FormInput
+            name={VenueFormConstants.NAMES.PHONE}
+            control={control}
+            label={VenueFormConstants.LABELS.PHONE}
+            defaultValue={venue.phone}
+            error={errors.phone?.message as string}
+            onChange={(e) => handleChange("phone", e.target.value)}
+          />
+          <FormInput
+            name={VenueFormConstants.NAMES.WEBSITE}
+            control={control}
+            label={VenueFormConstants.LABELS.WEBSITE}
+            defaultValue={venue.website}
+            error={errors.website?.message as string}
+            onChange={(e) => handleChange("website", e.target.value)}
+          />
+
+          <Box sx={{ pt: 3 }}>
+            <FormSelect
+              name={VenueFormConstants.NAMES.TYPE}
+              control={control}
+              label={VenueFormConstants.LABELS.TYPE}
+              options={venueTypes}
+              defaultValue={venue.type}
+              required
+              error={errors.type?.message as string}
+              onChange={(value) => handleChange("type", value)}
+            />
+          </Box>
+        </Box>
+
+        <Box sx={VenueFormStyles.rightBox}>
+          <Box sx={VenueFormStyles.fieldsBox}>
+            <Typography variant="h6">
+              {VenueFormConstants.LABELS.COUNTRY}
+            </Typography>
+            <FormAutoComplete
+              name={VenueFormConstants.NAMES.COUNTRY}
+              control={control}
+              label={VenueFormConstants.LABELS.COUNTRY}
+              defaultValue={venue.country}
+              options={VenueFormConstants.COUNTRIES}
+              required
+              error={errors.country?.message as string}
+              onChange={(value) => handleChange("country", value)}
+            />
+            <FormInput
+              name={VenueFormConstants.NAMES.CITY}
+              control={control}
+              label={VenueFormConstants.LABELS.CITY}
+              defaultValue={venue.city}
+              required
+              error={errors.city?.message as string}
+              onChange={(e) => handleChange("city", e.target.value)}
+            />
+            <FormInput
+              name={VenueFormConstants.NAMES.ADDRESS}
+              control={control}
+              label={VenueFormConstants.LABELS.ADDRESS}
+              defaultValue={venue.address}
+              multiline
+              rows={3}
+              required
+              error={errors.address?.message as string}
+              onChange={(e) => handleChange("address", e.target.value)}
+            />
+          </Box>
+
+          <Box sx={VenueFormStyles.buttonBox}>
+            <ButtonComponent
+              label="Attach to Event"
+              color="primary"
+              hoverColor="secondary"
+              size="medium"
+              onClickHandler={() => handleSubmit(onSubmit)()}
+            />
+          </Box>
+        </Box>
+      </form>
+    </Box>
+  );
+};
+
+export default VenueForm;
---
+++ app/frontend/src/components/VenueForm/VenueFormStyles.ts
@@ -0,0 +1,82 @@
+export const VenueFormStyles = {
+  formContainer: {
+    position: "relative",
+    width: "100vw",
+    height: "80vh",
+    maxWidth: "1000px",
+    maxHeight: "680px",
+    backgroundColor: "#f5f5f7",
+    borderRadius: "15px",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)",
+    display: "flex",
+    padding: "0 8%",
+
+    "@media (max-width: 768px)": {
+      position: "absolute",
+      display: "flex",
+      flexDirection: "row",
+      maxWidth: "90%",
+      maxHeight: "100%",
+      height: "85%",
+      overflowY: "scroll",
+    },
+  },
+
+  leftBox: {
+    flex: "0 1 50%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    maxWidth: "50%",
+    maxHeight: "100%",
+    paddingTop: "100px",
+    gap: "8px",
+    height: "100%",
+
+    "@media (max-width: 768px)": {
+      flex: "0 1 45%",
+      maxHeight: "40%",
+      paddingTop: "0px",
+      maxWidth: "90%",
+      width: "90%",
+      gap: "10px",
+    },
+  },
+
+  rightBox: {
+    flex: "0 1 40%",
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: "100px",
+    justifyContent: "flex-start",
+    maxWidth: "40%",
+    maxHeight: "100%",
+    gap: "8px",
+    height: "100%",
+
+    "@media (max-width: 768px)": {
+      flex: "0 1 45%",
+      maxHeight: "50%",
+      maxWidth: "90%",
+      width: "90%",
+    },
+  },
+
+  fieldsBox: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "8px",
+    width: "100%",
+  },
+
+  buttonBox: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
+
+    "@media (max-width: 768px)": {
+      alignItems: "center",
+      paddingBottom: "24px",
+    },
+  },
+};
