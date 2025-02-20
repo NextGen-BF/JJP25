1463ad7fe947288961bfa574ddcb4e4b42d435f0
PetarMitov <petar.mitov@blankfactor.com>
2025-02-20 13:57:12 +0200

    jjp-132: add email, username, firstName and lastName to the state

--- app/frontend/src/redux/slices/authSlice.ts
+++ app/frontend/src/redux/slices/authSlice.ts
@@ -2,12 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 import { registerUser } from "../services/authService";
 
 type AuthState = {
+  email: string | null;
+  username: string | null;
+  firstName: string | null;
+  lastName: string | null;
   loading: boolean;
   error: string | null;
   success: boolean;
 };
 
 const initialState: AuthState = {
+  email: null,
+  username: null,
+  firstName: null,
+  lastName: null,
   loading: false,
   error: null,
   success: false,
@@ -30,10 +38,15 @@ const authSlice = createSlice({
         state.error = null;
         state.success = false;
       })
-      .addCase(registerUser.fulfilled, (state) => {
+      .addCase(registerUser.fulfilled, (state, action) => {
         state.loading = false;
         state.error = null;
         state.success = true;
+        const { email, username, firstName, lastName } = action.payload;
+        state.email = email;
+        state.username = username;
+        state.firstName = firstName;
+        state.lastName = lastName;
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.loading = false;
