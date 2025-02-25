c75006b47bb754ea78599b239214418e9c8d289d
PetarMitov <petar.mitov@blankfactor.com>
2025-02-25 10:42:14 +0200

    jjp-155: remove commented code in GlobalExceptionHandler

--- app/backend/src/main/java/com/backend/exception/GlobalExceptionHandler.java
+++ app/backend/src/main/java/com/backend/exception/GlobalExceptionHandler.java
@@ -20,41 +20,11 @@ import java.util.stream.Collectors;
 @Log4j2
 public class GlobalExceptionHandler {
 
-    /* @ExceptionHandler()
-    public ResponseEntity<Map<String, String>> handleBadRequests(RuntimeException ex) {
-        log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
-        return new ResponseEntity<>(getErrorsMap("400", "BAD_REQUEST", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-    } */
-
-    /* @ExceptionHandler()
-    public ResponseEntity<Map<String, String>> handleUnauthorized(RuntimeException ex) {
-        log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
-        return new ResponseEntity<>(getErrorsMap("401", "UNAUTHORIZED", ex.getMessage()), new HttpHeaders(), HttpStatus.UNAUTHORIZED);
-    } */
-
-    /* @ExceptionHandler()
-    public ResponseEntity<Map<String, String>> handleForbidden(RuntimeException ex){
-        log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
-        return new ResponseEntity<>(getErrorsMap("403", "FORBIDDEN", ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
-    } */
-
-    /* @ExceptionHandler()
-    public ResponseEntity<Map<String, String>> handleNotFound(RuntimeException ex) {
-        log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
-        return new ResponseEntity<>(getErrorsMap("404", "NOT_FOUND", ex.getMessage()), new HttpHeaders(), HttpStatus.NOT_FOUND);
-    } */
-
     @ExceptionHandler({UserFoundException.class})
     public ResponseEntity<Map<String, String>> handleConflict(RuntimeException ex) {
         log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
         return new ResponseEntity<>(getErrorsMap("409", "CONFLICT", ex.getMessage()), new HttpHeaders(), HttpStatus.CONFLICT);
     }
-
-    /* @ExceptionHandler()
-    public ResponseEntity<Map<String, String>> handleGone(RuntimeException ex) {
-        log.error("Handled exception: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
-        return new ResponseEntity<>(getErrorsMap("410", "GONE", ex.getMessage()), new HttpHeaders(), HttpStatus.GONE);
-    } */
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Map<String, String>> handleInternalServerError(Exception ex) {
