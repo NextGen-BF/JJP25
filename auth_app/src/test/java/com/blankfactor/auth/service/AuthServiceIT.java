2feb47b56a69db4c425b6d7368faeb161daeeb9d
PetarMitov <petar.mitov@blankfactor.com>
2025-02-25 09:45:11 +0200

    jjp-155: update tests

--- auth_app/src/test/java/com/blankfactor/auth/service/AuthServiceIT.java
+++ auth_app/src/test/java/com/blankfactor/auth/service/AuthServiceIT.java
@@ -28,6 +28,19 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 public class AuthServiceIT {
 
+    private static final String TEST_EMAIL = "example@email.com";
+    private static final String TEST_PASSWORD = "Password1!";
+    private static final String TEST_USERNAME = "john_doe";
+    private static final String TEST_FIRST_NAME = "John";
+    private static final String TEST_LAST_NAME = "Doe";
+    private static final String TEST_BIRTHDATE = "2000-01-01T01:01:01";
+    private static final String TEST_ATTENDEE_ROLE = "attendee";
+    private static final String TEST_ORGANISER_ROLE = "organiser";
+    private static final String TEST_VERIFICATION_CODE = "123456";
+    private static final String VALID_TOKEN = "validToken";
+    private static final String NEW_PASSWORD = "newPassword1!";
+    private static final String DIFFERENT_PASSWORD = "differentPassword";
+
     private final AuthService authService;
     private final UserRepository userRepository;
 
@@ -47,56 +60,58 @@ public class AuthServiceIT {
         @Test
         void shouldSuccessfullyRegisterUser() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
 
             // When
             RegisterResponse registerResponse = authService.register(registerRequest);
 
             // Then
             assertTrue(userRepository.findByEmail(registerRequest.getEmail()).isPresent());
-            assertTrue(registerResponse.getEmail().equals(registerRequest.getEmail()));
-            assertTrue(registerResponse.getUsername().equals(registerRequest.getUsername()));
-            assertTrue(registerResponse.getFirstName().equals(registerRequest.getFirstName()));
-            assertTrue(registerResponse.getLastName().equals(registerRequest.getLastName()));
+            assertEquals(registerResponse.getEmail(), registerRequest.getEmail());
+            assertEquals(registerResponse.getUsername(), registerRequest.getUsername());
+            assertEquals(registerResponse.getFirstName(), registerRequest.getFirstName());
+            assertEquals(registerResponse.getLastName(), registerRequest.getLastName());
             assertFalse(registerResponse.getEnabled());
             assertNotNull(registerResponse.getRoles());
-            assertTrue(registerResponse.getRoles().size() == 1);
+            assertEquals(1, registerResponse.getRoles().size());
             assertTrue(registerResponse.getRoles().contains("ROLE_USER"));
         }
 
         @Test
         void shouldSuccessfullyRegisterAdmin() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "organiser");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ORGANISER_ROLE)
+                    .build();
 
             // When
             RegisterResponse registerResponse = authService.register(registerRequest);
 
             // Then
             assertTrue(userRepository.findByEmail(registerRequest.getEmail()).isPresent());
-            assertTrue(registerResponse.getEmail().equals(registerRequest.getEmail()));
-            assertTrue(registerResponse.getUsername().equals(registerRequest.getUsername()));
-            assertTrue(registerResponse.getFirstName().equals(registerRequest.getFirstName()));
-            assertTrue(registerResponse.getLastName().equals(registerRequest.getLastName()));
+            assertEquals(registerResponse.getEmail(), registerRequest.getEmail());
+            assertEquals(registerResponse.getUsername(), registerRequest.getUsername());
+            assertEquals(registerResponse.getFirstName(), registerRequest.getFirstName());
+            assertEquals(registerResponse.getLastName(), registerRequest.getLastName());
             assertFalse(registerResponse.getEnabled());
             assertNotNull(registerResponse.getRoles());
-            assertTrue(registerResponse.getRoles().size() == 2);
+            assertEquals(2, registerResponse.getRoles().size());
             assertTrue(registerResponse.getRoles().contains("ROLE_USER"));
             assertTrue(registerResponse.getRoles().contains("ROLE_ADMIN"));
         }
@@ -104,15 +119,16 @@ public class AuthServiceIT {
         @Test
         void shouldThrowUserFoundExceptionWhenEmailIsAlreadyInUse() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
 
             // When
             authService.register(registerRequest);
@@ -121,21 +137,22 @@ public class AuthServiceIT {
             UserFoundException exception = assertThrows(UserFoundException.class,
                     () -> authService.register(registerRequest));
 
-            assertEquals("example@email.com is already in use", exception.getMessage());
+            assertEquals(TEST_EMAIL + " is already in use", exception.getMessage());
         }
 
         @Test
         void shouldThrowUserFoundExceptionWhenUsernameIsAlreadyInUse() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
 
             // When
             authService.register(registerRequest);
@@ -145,21 +162,22 @@ public class AuthServiceIT {
             UserFoundException exception = assertThrows(UserFoundException.class,
                     () -> authService.register(registerRequest));
 
-            assertEquals("john_doe is already in use", exception.getMessage());
+            assertEquals(TEST_USERNAME + " is already in use", exception.getMessage());
         }
 
         @Test
         void shouldThrowPasswordDoNotMatchWhenPasswordsAreNotEqual() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password2!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword("Password2!")
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
 
             // When & Then
             PasswordsDoNotMatchException exception = assertThrows(PasswordsDoNotMatchException.class,
@@ -198,7 +216,7 @@ public class AuthServiceIT {
         @Test
         void shouldThrowUserNotFoundExceptionWhenEmailIsNotFound() {
             // Given
-            VerifyRequest verifyRequest = new VerifyRequest("not_found@email.com", "000000");
+            VerifyRequest verifyRequest = new VerifyRequest(TEST_EMAIL, TEST_VERIFICATION_CODE);
 
             // When & Then
             UserNotFoundException userNotFoundException = assertThrows(UserNotFoundException.class, () -> authService.verify(verifyRequest));
@@ -230,35 +248,37 @@ public class AuthServiceIT {
         @Test
         void shouldThrowIncorrectVerificationCodeExceptionWhenTheCodeIsIncorrect() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
             authService.register(registerRequest);
             VerifyRequest verifyRequest = new VerifyRequest(registerRequest.getEmail(), "000000");
 
             // When & Then
             IncorrectVerificationCodeException incorrectVerificationCodeException = assertThrows(IncorrectVerificationCodeException.class, () -> authService.verify(verifyRequest));
-            assertTrue(incorrectVerificationCodeException.getMessage().equals("Incorrect verification code: 000000"));
+            assertEquals("Incorrect verification code: 000000", incorrectVerificationCodeException.getMessage());
         }
 
         @Test
         void shouldThrowExpiredVerificationCodeExceptionWhenTheCodeHasExpired() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
             authService.register(registerRequest);
             User user = userRepository.findByEmail(registerRequest.getEmail()).get();
             String code = user.getVerificationCode();
@@ -277,15 +297,16 @@ public class AuthServiceIT {
         @Test
         void shouldSuccessfullyResendVerificationCode() {
             // Given
-            RegisterRequest registerRequest = new RegisterRequest(
-                    "example@email.com",
-                    "Password1!",
-                    "Password1!",
-                    "john_doe",
-                    "John",
-                    "Doe",
-                    LocalDateTime.parse("2000-01-01T01:01:01"),
-                    "attendee");
+            RegisterRequest registerRequest = RegisterRequest.builder()
+                    .email(TEST_EMAIL)
+                    .password(TEST_PASSWORD)
+                    .confirmPassword(TEST_PASSWORD)
+                    .username(TEST_USERNAME)
+                    .firstName(TEST_FIRST_NAME)
+                    .lastName(TEST_LAST_NAME)
+                    .birthDate(LocalDateTime.parse(TEST_BIRTHDATE))
+                    .role(TEST_ATTENDEE_ROLE)
+                    .build();
             RegisterResponse registerResponse = authService.register(registerRequest);
             String code = userRepository.findByEmail(registerRequest.getEmail()).get().getVerificationCode();
 
@@ -293,19 +314,16 @@ public class AuthServiceIT {
             String newCode = authService.resend(registerResponse.getEmail());
 
             // Then
-            assertFalse(code.equals(newCode));
-            assertTrue(userRepository.findByEmail(registerRequest.getEmail()).get()
-                    .getVerificationCode().equals(newCode));
+            assertNotEquals(code, newCode);
+            assertEquals(userRepository.findByEmail(registerRequest.getEmail()).get()
+                    .getVerificationCode(), newCode);
         }
 
         @Test
         void shouldThrowUserNotFoundExceptionWhenTheEmailIsNotFound() {
-            // Given
-            String email = "not_found@email.com";
-
-            // When & Then
-            UserNotFoundException userNotFoundException = assertThrows(UserNotFoundException.class, () -> authService.resend(email));
-            assertTrue(userNotFoundException.getMessage().equals(email + " is not found"));
+            // Given & When & Then
+            UserNotFoundException userNotFoundException = assertThrows(UserNotFoundException.class, () -> authService.resend(TEST_EMAIL));
+            assertEquals(TEST_EMAIL + " is not found", userNotFoundException.getMessage());
         }
 
         @Test // TODO: depends on ems_app, because makes an actual request
