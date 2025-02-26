3604d6eef0c0877f74d4c9ee65b8240a1da8f1b8
SezerMehmedBF <sezer.mehmed@blankfactor.com>
2025-02-26 16:15:42 +0200

    JJP-104: Add WebSocket for real-time payment notifications

--- app/backend/src/main/java/com/backend/service/notification/NotificationService.java
+++ app/backend/src/main/java/com/backend/service/notification/NotificationService.java
@@ -1,4 +1,7 @@
 package com.backend.service.notification;
 
+import com.backend.entity.dto.PaymentNotificationDTO;
+
 public interface NotificationService {
+    public void sendPaymentNotification(PaymentNotificationDTO notification);
 }
--- app/backend/src/main/java/com/backend/service/notification/NotificationServiceImpl.java
+++ app/backend/src/main/java/com/backend/service/notification/NotificationServiceImpl.java
@@ -1,7 +1,19 @@
 package com.backend.service.notification;
 
+import com.backend.entity.dto.PaymentNotificationDTO;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
+
+    private static final String NOTIFICATION_DESTINATION = "/topic/notifications";
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendPaymentNotification(PaymentNotificationDTO notification) {
+        messagingTemplate.convertAndSend(NOTIFICATION_DESTINATION, notification);
+    }
 }
